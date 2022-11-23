2c9effa2bd0a361e6709aa1599c12a688fea329d
Anbcodes <me@anb.codes>
2022-11-22 21:26:13 -0500

    redirect to http from https

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -21,6 +21,10 @@
 	let send = false;
 
 	onMount(() => {
+		if (window.location.protocol === 'https:') {
+			window.location.protocol = 'http:';
+		}
+
 		equations = JSON.parse(localStorage.getItem('equations') || '[]');
 		equations.forEach((eq) => {
 			eq.h.onChange = onChangeFunc(eq.h, eq);
